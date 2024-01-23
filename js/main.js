f846c26497b6c4e2b00615a46e47334b0215881c
WestSopho <isaiasvelascoricardo@gmail.com>
2024-01-23 13:44:16 -0500

    header done

--- js/main.js
+++ js/main.js
@@ -4,21 +4,21 @@
 
 var imageHero = document.getElementById("image-hero");
 var header = document.querySelector("header");
+var main = document.querySelector("main");
 var headerPadding = header.style.paddingBottom;
-
+var mainPadding = header.style.paddingTop
 
 function adjustHero(){
     var height = document.getElementById("image-hero").offsetHeight;
     console.log(height);
     header.style.paddingBottom = headerPadding + (height / 2)  + "px";
+    main.style.paddingTop = mainPadding+ (height / 2) + 'px';
 }
 
 window.addEventListener("load", function(){
-    console.log("ayuda load");
     adjustHero();
 })
 
 window.addEventListener("resize", function(){
-    console.log("ayuda resize");
     adjustHero();
 })
